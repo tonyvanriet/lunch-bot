25df91edbd5821be4914176aca4a31eb3934eca6
Tony van Riet <tonyvanriet@gmail.com>
2015-01-11 23:43:58 -0600

    handles slack events

--- src/lunch_bot/core.clj
+++ src/lunch_bot/core.clj
@@ -1,6 +1,10 @@
 (ns lunch-bot.core
   (:gen-class)
-  (:require [clj-slack-client.core :as slack]))
+  (:require
+   [clj-slack-client
+    [core :as slack]
+    [team-state :as state]
+    [rtm-transmit :as tx]]))
 
 
 (def abot-api-token "xoxb-3215140999-UuVgqNVwxMDcWNrVeoOMMtxw")
@@ -8,10 +12,28 @@
 (def tonyvanriet-api-token "xoxp-3215134233-3215134235-3216767432-ca2d3d")
 
 
-(defn shutdown-app []
-  (slack/disconnect)
-  (shutdown-agents)
-  (println "lunch-bot dying"))
+(defmulti handle-event :type)
+
+
+(defmethod handle-event "message"
+  [event]
+  (let [user-id (:user event)
+        user (state/get-user user-id)
+        self-id (:id (state/get-self))
+        channel-id (:channel event)]
+    (when (and (not= user-id self-id)
+               (not (:is_bot user)))
+      (tx/say-message channel-id "That's what she said"))))
+
+
+(defmethod handle-event "channel_joined"
+  [event]
+  nil)
+
+
+(defmethod handle-event :default
+  [event]
+  nil)
 
 
 (defn wait-for-console-quit []
@@ -21,12 +43,18 @@
           (recur)))))
 
 
+(defn shutdown-app []
+  (slack/disconnect)
+  (shutdown-agents)
+  (println "lunch-bot dying"))
+
+
 (defn -main
   [& args]
 
   (try
 
-    (slack/connect abot-api-token)
+    (slack/connect abot-api-token handle-event)
     (println "lunch-bot running...")
 
     (wait-for-console-quit)
