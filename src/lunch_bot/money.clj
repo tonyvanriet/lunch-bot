06712389b3aaed6021415cd26f5714497cbc6ed3
tonyvanriet <tonyvanriet@gmail.com>
2015-01-25 13:39:16 -0600

    refactor balance calculation

--- src/lunch_bot/money.clj
+++ src/lunch_bot/money.clj
@@ -8,11 +8,11 @@
 
 
 (defn event->balance-changes
-  [event]
-  (cond (= (:type event) :bought) (balance-change (:person event) (:amount event))
-        (= (:type event) :cost) (balance-change (:person event) (* -1 (:amount event)))
-        (= (:type event) :paid) [(balance-change (:person event) (:amount event))
-                                 (balance-change (:recipient event) (* -1 (:amount event)))]))
+  [{:keys [type person recipient amount]}]
+  (cond (= type :bought) (balance-change person amount)
+        (= type :cost)   (balance-change person (* -1 amount))
+        (= type :paid)   [(balance-change person amount)
+                          (balance-change recipient (* -1 amount))]))
 
 
 (defn events->balance-changes
@@ -22,27 +22,18 @@
        (flatten)))
 
 
-(defn balances->balance
-  [balances person]
-  (first (filter #(= :person person) balances)))
-
-
-(defn update-balances
-  [balances balance-change]
-  (let [person (:person balance-change)
-        pre-balance (get balances person 0)]
-    (assoc balances person (+ pre-balance (:amount balance-change)))))
-
-
 (defn events->balances
   "turns a stream of events into a map of balances for each person"
   [events]
-  (let [balance-changes (events->balance-changes events)]
-    (reduce update-balances {} balance-changes)))
-
+  (let [balance-changes (events->balance-changes events)
+        balance-changes-by-person (group-by #(:person %) balance-changes)]
+    (into {} (for [[person balance-changes] balance-changes-by-person]
+               [person (reduce + (map #(:amount %) balance-changes))]))))
 
 
 (defn minimize-debt
   [events]
   nil)
+
+
 
--- test/lunch_bot/core_test.clj
+++ test/lunch_bot/core_test.clj
@@ -15,10 +15,16 @@
    {:person "dude"
     :type :cost
     :amount 8}
+   {:person "rozz"
+    :type :cost
+    :amount 12}
+   {:person "bob"
+    :type :cost
+    :amount 10}
 
    {:person "steve"
     :type :paid
-    :amount 20
+    :amount 10
     :recipient "bob"}
    {:person "dude"
     :type :paid
@@ -31,20 +37,19 @@
    {:person "steve"
     :type :cost
     :amount 7}
-   {:person "dude"
+   {:person "rozz"
     :type :cost
-    :amount 7}
-
-   {:person "dude"
-    :type :paid
-    :amount 7
-    :recipient "bob"}])
+    :amount 4}
+   {:person "bob"
+    :type :cost
+    :amount 9}])
 
 
 (deftest create-balances-from-events
   (let [balances (money/events->balances money-events)]
-    (is (= (get balances "bob") 25))
-    (is (= (get balances "steve") 3))
-    (is (= (get balances "dude") 0))))
+    (is (= (get balances "bob") 23))
+    (is (= (get balances "steve") -7))
+    (is (= (get balances "dude") 0))
+    (is (= (get balances "rozz") -16))))
 
 
