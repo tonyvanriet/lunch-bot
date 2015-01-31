cb4157253f3a83cef5e5cc6cd140a90ff3421b1b
tonyvanriet <tonyvanriet@gmail.com>
2015-01-31 15:01:55 -0600

    core passes message handling to communicate

--- src/lunch_bot/core.clj
+++ src/lunch_bot/core.clj
@@ -1,10 +1,10 @@
 (ns lunch-bot.core
   (:gen-class)
   (:require
-   [clj-slack-client
-    [core :as slack]
-    [team-state :as state]
-    [rtm-transmit :as tx]]))
+    [lunch-bot.communicate :as comm]
+    [clj-slack-client
+     [core :as slack]
+     [team-state :as state]]))
 
 
 (def api-token (->> "api-token.txt"
@@ -12,31 +12,15 @@
                     (clojure.string/trim)))
 
 
-(defn handle-command
-  [channel-id event]
-  (tx/say-message channel-id (:text event)))
-
-
 (defmulti handle-event :type)
 
-(defmethod handle-event "message"
-  [event]
-  (let [user-id (:user event)
-        user (state/id->user user-id)
-        self-id (:id (state/self))
-        channel-id (:channel event)]
-    (when (and (not= user-id self-id)
-               (not (:is_bot user))
-               (:text event)
-               (re-matches #"lunch .*" (:text event)))
-      (handle-command channel-id event))))
+(defmethod handle-event "message" [event]
+  (comm/handle-message event))
 
-(defmethod handle-event "channel_joined"
-  [event]
+(defmethod handle-event "channel_joined" [event]
   nil)
 
-(defmethod handle-event :default
-  [event]
+(defmethod handle-event :default [event]
   nil)
 
 
