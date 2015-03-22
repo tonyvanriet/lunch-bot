4e8142999ab8f5d90ed2cb4f4355cc5944c469b0
tonyvanriet <tonyvanriet@gmail.com>
2015-03-22 00:59:41 -0500

    fix use of today in meal-test

--- test/lunch_bot/meal_test.clj
+++ test/lunch_bot/meal_test.clj
@@ -8,7 +8,7 @@
   (swap! events #(conj % event)))
 
 (defn get-person-meal-today [events person]
-  (get-in (events->meals events) [time/today :people person]))
+  (get-in (events->meals events) [(time/today) :people person]))
 
 
 (deftest meal-participation
@@ -19,17 +19,17 @@
     ; opportunity for a with-events macro here?
     ; to facilitate the accumulation of events throughout the test
     (testing "meal shows person in"
-      (append-event meal-events {:type :in, :person person, :date time/today})
+      (append-event meal-events {:type :in, :person person, :date (time/today)})
       (let [person-meal (get-person-meal-today @meal-events person)]
         (is (= :in (:status person-meal)))))
     (testing "meal shows order and cost"
-      (append-event meal-events {:type :order, :person person, :date time/today, :food order})
-      (append-event meal-events {:type :cost, :person person, :date time/today, :amount cost-amount})
+      (append-event meal-events {:type :order, :person person, :date (time/today), :food order})
+      (append-event meal-events {:type :cost, :person person, :date (time/today), :amount cost-amount})
       (let [person-meal (get-person-meal-today @meal-events person)]
         (is (= order (:order person-meal)))
         (is (= cost-amount (:cost person-meal)))))
     (testing "meal shows person out, no order and cost"
-      (append-event meal-events {:type :out, :person person, :date time/today})
+      (append-event meal-events {:type :out, :person person, :date (time/today)})
       (let [person-meal (get-person-meal-today @meal-events person)]
         (is (= :out (:status person-meal)))
         (is (not (contains? person-meal :order)))
