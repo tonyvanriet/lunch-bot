a195902291f8bea6bc1147c2264faab2aa3d334b
Tony van Riet <tonyvanriet@gmail.com>
2015-05-01 00:00:46 -0500

    update project description and dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject lunch-bot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A Slack bot to manage group lunches"
+  :url ""
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [clj-slack-client "0.1.0-SNAPSHOT"]
+                 [clj-slack-client "0.1.1-SNAPSHOT"]
                  [clj-time "0.9.0"]
                  [org.clojure/math.combinatorics "0.0.8"]
                  [org.clojure/core.incubator "0.1.3"]]
