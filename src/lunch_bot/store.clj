ed5a15fd5bc495a9ad2496e3582ceea1dd85b0ee
Tony van Riet <tonyvanriet@gmail.com>
2015-06-14 23:10:25 -0500

    factor out slurp-filename

--- src/lunch_bot/store.clj
+++ src/lunch_bot/store.clj
@@ -4,13 +4,20 @@
             [clojure.edn :as edn]))
 
 
-(defn read-api-token
+(defn slurp-filename
   [filename]
   (let [file (io/file filename)]
-    (if (.exists file)
-      (string/trim (slurp file))
-      (do (spit filename "your-api-token")
-          (println "put your api token in" filename)))))
+    (when (and (.exists file)
+               (< 0 (.length file)))
+      (slurp file))))
+
+
+(defn read-api-token
+  [filename]
+  (if-let [raw-api-token (slurp-filename filename)]
+    (string/trim raw-api-token)
+    (do (spit filename "your-api-token")
+        (println "put your api token in" filename))))
 
 
 (defn- date->inst [date] (when date (.toDate date)))
@@ -36,12 +43,9 @@
 
 (defn read-events
   [filename]
-  (let [file (io/file filename)]
-    (when (and (.exists file)
-               (< 0 (.length file)))
-      (->> file
-           (slurp)
-           (edn->events)))))
+  (->> filename
+       (slurp-filename)
+       (edn->events)))
 
 
 (defn write-events
